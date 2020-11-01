93c233e816ba6973dd4e483894ff2779e029cf7b
Dmitry Davydov <davidov.dima@gmail.com>
2020-11-02 01:12:37 +0300

    initial

---
+++ TabataPlus/TabataPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabataPlusApp.swift
+//  TabataPlus
+//
+//  Created by Дима Давыдов on 31.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct TabataPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            InProgress()
+        }
+    }
+}
